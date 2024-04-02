9615d29ca97774f5c3aca5cf6752332e3d6c8cc3
Isaac <isaacspiegel@gmail.com>
2024-04-02 16:33:09 -0500

    Add queue headers

---
+++ queue.h
@@ -0,0 +1,26 @@
+#include "list.cpp"
+#include <cstddef>
+
+template <typename Item_Type>
+class Queue
+{
+  private:
+    std::size_t num_items;
+    Node<Item_Type>* front_of_queue;
+    Node<Item_Type>* back_of_queue;
+  public:
+    // Constructor
+    Queue();
+    // Destructor
+    ~Queue();
+    // Inserts a copy of item at the back of the queue.
+    void push(const Item_Type& item);
+    // Removes the first item from the queue and returns it.
+    Item_Type pop();
+    // Gets the first element in the queue.
+    Item_Type front();
+    // Returns true if this queue is empty.
+    bool empty() const;
+    // Returns the number of items in the queue.
+    std::size_t size() const;
+};
